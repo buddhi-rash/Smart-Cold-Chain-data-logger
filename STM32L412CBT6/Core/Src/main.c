ae4a4104da0f9631d810b4260f33826ee43c0b35
buddhi-rash <kumarabuddhi231001@gmail.com>
2026-07-04 17:17:55 +0530

    Resolve merge conflict and update STM32 project files

--- STM32L412CBT6/Core/Src/main.c
+++ STM32L412CBT6/Core/Src/main.c
@@ -234,6 +234,18 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
+  /*Configure GPIO pins Output Level */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1 | GPIO_PIN_4, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : PA1 (status LED) and PA4 (MAX31865 CS) */
+  GPIO_InitStruct.Pin = GPIO_PIN_1 | GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
   /* USER CODE END MX_GPIO_Init_2 */
