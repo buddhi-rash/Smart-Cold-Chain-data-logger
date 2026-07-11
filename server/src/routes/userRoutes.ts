c43cf8c605782dcdd86ffc0b0915d4d4058f2887
buddhi-rash <kumarabuddhi231001@gmail.com>
2026-07-11 09:04:07 +0530

    Del

--- server/src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { changePassword } from '../controllers/userController';
-import { requireAuth } from '../middleware/auth';
-
-const router = Router();
-
-router.post('/me/password', requireAuth, changePassword);
-
-export default router;
